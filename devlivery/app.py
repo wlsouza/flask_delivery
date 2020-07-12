94ceb763b91bc0f1dcb15d5e5ca4907b020639e5
Wellington Lorenço <wlsouza@protonmail.com>
2020-07-12 15:19:51 -0300

    feat[site]: create index and base template

--- devlivery/app.py
+++ devlivery/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
+from devlivery.ext import site
 
 def create_app():
     app = Flask(__name__)
+    site.init_app(app)
     return app
--- devlivery/ext/site/main.py
+++ devlivery/ext/site/main.py
@@ -3,9 +3,7 @@ from flask import Blueprint
 
 bp = Blueprint("site", __name__)
 
+
 @bp.route("/")
 def index():
     return render_template("index.html")
-
-
-
